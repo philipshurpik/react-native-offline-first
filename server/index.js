4c2dc57847b150a5e40abe7820a7e7176e38cd45
philipshurpik <philip.shurpik@gmail.com>
2016-05-25 09:42:11 +0300

    store server data in json file

--- server/index.js
+++ server/index.js
@@ -1,26 +1,36 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var jsonfile = require('jsonfile');
+var file = './data.json';
+
+var todos = [];
+try {
+	todos = jsonfile.readFileSync(file);
+}
+catch (e) {
+	saveTodos();
+}
 
 var app = express();
 app.use(bodyParser.json());
 app.post('/todos', validate);
 app.put('/todos/:id', validate);
 
-var todos = [];
-
-app.get('/todos', function(req, res) {
+app.get('/todos', function (req, res) {
 	res.json(todos);
 });
 
-app.post('/todos', function(req, res) {
+app.post('/todos', function (req, res) {
 	todos.push(req.body);
+	saveTodos();
 	res.json(req.body);
 });
 
-app.put('/todos/:id', function(req, res) {
+app.put('/todos/:id', function (req, res) {
 	const item = todos.find(item => item.id === req.body.id);
 	const index = todos.indexOf(item);
 	todos[index] = req.body;
+	saveTodos();
 	res.json(req.body);
 });
 
@@ -33,6 +43,10 @@ function validate(req, res, next) {
 		return res.sendStatus(400);
 	}
 	next();
+}
+
+function saveTodos() {
+	jsonfile.writeFileSync(file, todos);
 }
 
 app.listen(1906);
